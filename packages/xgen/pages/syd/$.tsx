df6db2393c065aabe4f1af7c7bc182a0d22b04a5
hi34568254@aliyun.com <hi34568254@aliyun.com>
2024-11-21 22:02:09 +0800

    调用接口获取页面信息

--- packages/xgen/pages/syd/$.tsx
+++ packages/xgen/pages/syd/$.tsx
@@ -1,10 +1,11 @@
 import { useMatch } from '@/hooks'
 import { history, useSearchParams } from '@umijs/max'
 import axios from 'axios'
-import type { Global } from '@/types'
+import type { Global, Response } from '@/types'
 
 /** Dynamically forward to the components */
-const Index = () => {
+const Index = async () => {
+	const locale = getLocale()
 	const [params] = useSearchParams()
 	const search_params = Object.fromEntries(params)
 
@@ -13,26 +14,15 @@ const Index = () => {
 		['type', 'model', 'id', 'formType']
 	)
 
+	const getPageSchema = async () => {
+		const schema = await axios.get<Global.AnyObject, Response<Global.AnyObject>>(`/api/v1/syd/schema/${type}/${model}`)
+		return schema
+	}	
+
 	if (!model) history.push('/404')
-	const html =  `type=${type},model=${model},id=${id},formType=${formType},search_params=${search_params}`
-
-	// return axios.get<Req, Response<Res>>(`/api/${window.$app.api_prefix}/table/${model}/search`, { params })
-	// axios.post<{}, any>(`/api/__yao/app/service/${name}`, payload)
-	// 		.then((res) => {
-	// 			if (res.code && res.message && res.code == 200) {
-					
-	// 			}
-	// 		})
-
-
-	// const { res, err } = await this.service.search<TableType.SearchParams, TableType.Data>(
-	// 	this.model,
-	// 	this.search_params
-	// )
-
-
+	
 	return (
-		<div>{html}</div>
+		<div>{await getPageSchema()}</div>
 	)
 }
 
