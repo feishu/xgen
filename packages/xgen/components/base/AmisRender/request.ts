9be8bd045097c8e64eb82556a4c2218de66acfe8
hi34568254@aliyun.com <hi34568254@aliyun.com>
2024-12-10 11:42:18 +0800

    网络请求格式做了调整

--- packages/xgen/components/base/AmisRender/request.ts
+++ packages/xgen/components/base/AmisRender/request.ts
@@ -1,45 +1,54 @@
 import axios from 'axios'
 import type { Global, Response } from '@/types'
 import { makeTranslator, attachmentAdpator } from 'amis-core'
+import { message } from 'antd'
 export const amisRequest = (api,locale:string) => {
     const { method, url, data } = api
     const check = (response, api) => {
         const __ = makeTranslator(locale || 'zh-CN');
-        response = attachmentAdpator(response, __, api);
+        let _response  = { 
+            data : { 
+                status: response && (response.data?.code || response?.code) || 200,
+                msg: response && (response.data?.message || response.data?.msg || response.message || response.msg) || "处理成功",
+                data: response && (response.data === undefined || response.data === null || response.data === '') ? response : response.data 
+            } 
+        };
+
+        _response = attachmentAdpator(_response, __, api);
         //判断返回结构是否已经是amis结构
         if (
-          typeof response.data === "object" &&
-          response.data !== null &&
-          "data" in response.data &&
-          "msg" in response.data &&
-          "status" in response.data
+          typeof _response.data === "object" &&
+          _response.data !== null &&
+          "data" in _response.data &&
+          "msg" in _response.data &&
+          "status" in _response.data
         ) {
           return new Promise(function (resolve, reject) {
-            resolve(response);
+            resolve(_response);
           });
         }
     
         if (!url.startsWith("/api")) {
           return new Promise(function (resolve, reject) {
-            resolve(response);
+            resolve(_response);
           });
         }
     
         //组成新的payload,即是修改response的数据
         let payload = {
-          status: !response.data?.code ? 200 : response.data.code,
-          msg: response.data?.message ? response.data?.message ?? response.data.msg : "处理成功",
-          data: response.data,
+          status: _response.data?.status,
+          msg: _response.data?.msg,
+          data: _response.data,
         };
     
-        response.data = payload;
+        _response.data = payload;
     
         // 在这个回调函数中返回一个新的 Promise 对象
         return new Promise(function (resolve, reject) {
           // 这里应该返回一个新的response,可以在下一个adapter里使用
           // 执行异步操作
           // 在异步操作完成后调用 resolve 或 reject
-          resolve(response);
+          resolve(_response);
         });
     };
     return axios[method]<Global.AnyObject, Response<Global.AnyObject>>(url, data)
